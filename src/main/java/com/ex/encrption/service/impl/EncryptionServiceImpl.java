e3dd8b0bf9444ac775370bf83002de66c584dfa4
janakiramdudu11 <138783831+janakiramdudu11@users.noreply.github.com>
2023-07-27 12:13:24 +0530

    Update EncryptionServiceImpl.java

--- src/main/java/com/ex/encrption/service/impl/EncryptionServiceImpl.java
+++ src/main/java/com/ex/encrption/service/impl/EncryptionServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ex.encrption.service.impl;
 
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -119,6 +120,48 @@ public class EncryptionServiceImpl implements EncryptionService {
 		token.setpassword(aes.decrypt(String.valueOf(body.get("password"))));
 		token.setExpiryInMinutes(Integer.parseInt(String.valueOf(body.get("expirationTime"))));
 		return token;
+	}
+	
+
+	private static final String ALGO = "AES";
+
+	// Default uses ECB PKCS5Padding"
+
+	public  String encrypted(String Data, String secret) throws Exception {
+		Key key = generateKey(secret);
+		Cipher c = Cipher.getInstance(ALGO);
+		c.init(Cipher.ENCRYPT_MODE, key);
+		byte[] encVal = c.doFinal(Data.getBytes());
+		String encryptedValue = Base64.getEncoder().encodeToString(encVal);
+		return encryptedValue;
+	}
+
+	public  String decrypted(String strToDecrypt, String secret) {
+		try {
+			Key key = generateKey(secret);
+			Cipher cipher = Cipher.getInstance(ALGO);
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+		} catch (Exception e) {
+			System.out.println("Error while decrypting: " + e.toString());
+		}
+		return null;
+	}
+
+	private  Key generateKey(String secret) throws Exception {
+		byte[] decoded = Base64.getDecoder().decode(secret.getBytes());
+		Key key = new SecretKeySpec(decoded, ALGO);
+		return key;
+	}
+
+	public  String decodeKey(String str) {
+		byte[] decoded = Base64.getDecoder().decode(str.getBytes());
+		return new String(decoded);
+	}
+
+	public String encodeKey(String str) {
+		byte[] encoded = Base64.getEncoder().encode(str.getBytes());
+		return new String(encoded);
 	}
 
 }
